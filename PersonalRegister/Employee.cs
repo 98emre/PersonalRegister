ce27494b37a4a7a5316df8ffb24890b1f70e8c03
98emre <emre-98@live.se>
2024-02-27 14:39:38 +0100

    feat: added Employee class

---
+++ PersonalRegister/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister
+{
+    public class Employee
+    {
+        private string name;
+        private int salary;
+
+        public Employee(string name, int salary) { 
+            this.name = name;
+            this.salary = salary;   
+        }   
+
+        public void setName(string name) { 
+            this.name = name;
+        }
+
+        public string getName() { 
+            return this.name;
+        }
+
+        public void setSalary(int salary){   
+            this.salary = salary;
+        }
+
+        public int getSalary() { 
+            return this.salary;
+        }
+
+        public String toString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.name);
+            sb.Append(" ");
+            sb.Append(this.salary);
+
+            return sb.ToString();
+        }
+
+    }
+}
